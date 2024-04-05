30110a8628654d6a959913a30156e2b5aa735432
Eyuelb <eyuelbelete98@gmail.com>
2024-04-05 22:53:03 +0300

    mantine improvement

--- lib/mantine/component/toggle-button/index.tsx
+++ lib/mantine/component/toggle-button/index.tsx
@@ -6,32 +6,33 @@ import {
   useMantineColorScheme,
 } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons-react";
-import React from "react";
+import React, { memo, useMemo } from "react";
 import { useIsMounted } from "../../hooks/useIsMounted";
 
 type Props = ActionIconProps;
 
-const ThemeToggleButton = (props: Props) => {
+const ThemeToggleButton = memo((props: Props) => {
   const isMounted = useIsMounted();
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme("light", {
     getInitialValueInEffect: true,
   });
+  const color = useMemo(()=>computedColorScheme,[computedColorScheme])
   return (
     <React.Fragment>
       {isMounted ? (
         <ActionIcon
           onClick={() =>
-            setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+            setColorScheme(color === "light" ? "dark" : "light")
           }
           variant="transparent"
           size="sm"
           {...props}
         >
-          {computedColorScheme == "dark" && (
+          {color == "dark" && (
             <IconSun size={20} color="yellow" stroke={1.8} />
           )}
-          {computedColorScheme == "light" && (
+          {color == "light" && (
             <IconMoon size={20} color="gray" stroke={1.8} />
           )}
         </ActionIcon>
@@ -40,6 +41,7 @@ const ThemeToggleButton = (props: Props) => {
       )}
     </React.Fragment>
   );
-};
+});
+ThemeToggleButton.displayName = "ThemeToggleButton";
 
 export default ThemeToggleButton;
